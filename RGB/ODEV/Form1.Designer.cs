5d7709813fa7d68d3c09bf7005a3977daee2f90c
huseyinorcan24 <48436868+huseyinorcan24@users.noreply.github.com>
2022-04-16 00:56:50 +0300

    RGB

---
+++ RGB/ODEV/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ODEV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pt_resim = new System.Windows.Forms.PictureBox();
+            this.pt_YResim = new System.Windows.Forms.PictureBox();
+            this.btn_piksel = new System.Windows.Forms.Button();
+            this.lbl_gri = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pt_resim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pt_YResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pt_resim
+            // 
+            this.pt_resim.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pt_resim.Location = new System.Drawing.Point(25, 27);
+            this.pt_resim.Name = "pt_resim";
+            this.pt_resim.Size = new System.Drawing.Size(299, 373);
+            this.pt_resim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pt_resim.TabIndex = 1;
+            this.pt_resim.TabStop = false;
+            this.pt_resim.Click += new System.EventHandler(this.pt_resim_Click);
+            // 
+            // pt_YResim
+            // 
+            this.pt_YResim.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pt_YResim.Location = new System.Drawing.Point(350, 27);
+            this.pt_YResim.Name = "pt_YResim";
+            this.pt_YResim.Size = new System.Drawing.Size(301, 373);
+            this.pt_YResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pt_YResim.TabIndex = 6;
+            this.pt_YResim.TabStop = false;
+            this.pt_YResim.Click += new System.EventHandler(this.pt_YResim_Click);
+            // 
+            // btn_piksel
+            // 
+            this.btn_piksel.Location = new System.Drawing.Point(585, 415);
+            this.btn_piksel.Name = "btn_piksel";
+            this.btn_piksel.Size = new System.Drawing.Size(75, 23);
+            this.btn_piksel.TabIndex = 7;
+            this.btn_piksel.Text = "Dönüşüm";
+            this.btn_piksel.UseVisualStyleBackColor = true;
+            this.btn_piksel.Click += new System.EventHandler(this.btn_piksel_Click);
+            // 
+            // lbl_gri
+            // 
+            this.lbl_gri.AutoSize = true;
+            this.lbl_gri.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_gri.Location = new System.Drawing.Point(423, 415);
+            this.lbl_gri.Name = "lbl_gri";
+            this.lbl_gri.Size = new System.Drawing.Size(41, 26);
+            this.lbl_gri.TabIndex = 8;
+            this.lbl_gri.Text = "G: ";
+            this.lbl_gri.Click += new System.EventHandler(this.lbl_gri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 450);
+            this.Controls.Add(this.lbl_gri);
+            this.Controls.Add(this.btn_piksel);
+            this.Controls.Add(this.pt_YResim);
+            this.Controls.Add(this.pt_resim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pt_resim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pt_YResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pt_resim;
+        private System.Windows.Forms.PictureBox pt_YResim;
+        private System.Windows.Forms.Button btn_piksel;
+        private System.Windows.Forms.Label lbl_gri;
+    }
+}
+
---
+++ RGB/ODEV/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ODEV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pt_resim_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaEkle = new OpenFileDialog();
+            dosyaEkle.Filter = "Jpeg Dosyası|*.jpeg;*.jpg|Bmp Dosyası|*.bmp";
+
+            if (dosyaEkle.ShowDialog() == DialogResult.OK)
+            {
+                pt_resim.ImageLocation = dosyaEkle.FileName;
+            }
+        }
+
+        private void pt_YResim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_piksel_Click(object sender, EventArgs e)
+        {
+            if (pt_resim.Image == null) return;
+
+            Bitmap resim = (Bitmap)pt_resim.Image;
+            Color ilkPiksel = resim.GetPixel(0, 0);
+
+            int genislik = resim.Width;
+            int yukseklik = resim.Height;
+
+            Bitmap yeniResim = new Bitmap(genislik, yukseklik);
+            //Buradaki İç içe  2 For döngümüz resim üzerinde pixel pixel gezmemizi sağlar
+            for (int i = 0; i < resim.Width; i++)
+            {
+                for (int x = 0; x < resim.Height; x++)
+                {
+                    Color oc = resim.GetPixel(i, x);
+                    int grayScale = (int)((oc.R * 0.3) + (oc.G * 0.59) + (oc.B * 0.11));
+                    Color nc = Color.FromArgb(oc.A, grayScale, grayScale, grayScale);
+                    yeniResim.SetPixel(i, x, nc);
+                }
+            }
+            pt_YResim.Image = yeniResim;//tüm işlemler bittikten sonrada  nesmize yeni resmimizi aktarırız.
+           lbl_gri.Text = "G: " + ilkPiksel.B.ToString();
+        }
+
+        private void lbl_gri_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
